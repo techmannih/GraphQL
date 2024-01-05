3056313216fd436e94b5007b7b9cbaf2dffcf502
techmannih <manishchaudharyttt@gmail.com>
2024-01-05 17:50:11 +0530

    add id in data

--- server/data.js
+++ server/data.js
@@ -1,52 +1,57 @@
+// const peopleData.js
 const peopleData = [
-    {
-      name: "John Doe",
-      age: 25,
-      city: "NEWYORK",
-      friends: [
-        { name: "Jane Smith", age: 30, city: "LOS ANGELES" },
-        { name: "Bob Johnson", age: 28, city: "CHICAGO" },
-      ],
-    },
-    {
-      name: "Jane Smith",
-      age: 30,
-      city: "LOSANGELES",
-      friends: [
-        { name: "John Doe", age: 25, city: "NEW YORK" },
-        { name: "Bob Johnson", age: 28, city: "CHICAGO" },
-        { name: "Alice Brown", age: 35, city: "SEATTLE" },
-      ],
-    },
-    {
-      name: "Bob Johnson",
-      age: 28,
-      city: "CHICAGO",
-      friends: [
-        { name: "John Doe", age: 25, city: "NEW YORK" },
-        { name: "Jane Smith", age: 30, city: "LOS ANGELES" },
-        { name: "Charlie Wilson", age: 22, city: "MIAMI" },
-      ],
-    },
-    {
-      name: "Alice Brown",
-      age: 35,
-      city: "SEATTLE",
-      friends: [
-        { name: "Jane Smith", age: 30, city: "LOS ANGELES" },
-        { name: "Charlie Wilson", age: 22, city: "MIAMI" },
-      ],
-    },
-    {
-      name: "Charlie Wilson",
-      age: 22,
-      city: "MIAMI",
-      friends: [
-        { name: "Bob Johnson", age: 28, city: "CHICAGO" },
-        { name: "Alice Brown", age: 35, city: "SEATTLE" },
-      ],
-    },
-  ];
-  
-  module.exports = { peopleData };
-  
+  {
+    id: "1",
+    name: "John Doe",
+    age: 25,
+    city: "NEWYORK",
+    friends: [
+      { id: "2", name: "Jane Smith", age: 30, city: "LOSANGELES" },
+      { id: "3", name: "Bob Johnson", age: 28, city: "CHICAGO" },
+    ],
+  },
+  {
+    id: "2",
+    name: "Jane Smith",
+    age: 30,
+    city: "LOSANGELES",
+    friends: [
+      { id: "1", name: "John Doe", age: 25, city: "NEWYORK" },
+      { id: "3", name: "Bob Johnson", age: 28, city: "CHICAGO" },
+      { id: "4", name: "Alice Brown", age: 35, city: "SEATTLE" },
+    ],
+  },
+  {
+    id: "3",
+    name: "Bob Johnson",
+    age: 28,
+    city: "CHICAGO",
+    friends: [
+      { id: "1", name: "John Doe", age: 25, city: "NEWYORK" },
+      { id: "2", name: "Jane Smith", age: 30, city: "LOSANGELES" },
+      { id: "5", name: "Charlie Wilson", age: 22, city: "MIAMI" },
+    ],
+  },
+  {
+    id: "4",
+    name: "Alice Brown",
+    age: 35,
+    city: "SEATTLE",
+    friends: [
+      { id: "2", name: "Jane Smith", age: 30, city: "LOSANGELES" },
+      { id: "5", name: "Charlie Wilson", age: 22, city: "MIAMI" },
+    ],
+  },
+  {
+    id: "5",
+    name: "Charlie Wilson",
+    age: 22,
+    city: "MIAMI",
+    friends: [
+      { id: "3", name: "Bob Johnson", age: 28, city: "CHICAGO" },
+      { id: "4", name: "Alice Brown", age: 35, city: "SEATTLE" },
+    ],
+  },
+];
+
+module.exports = { peopleData };
