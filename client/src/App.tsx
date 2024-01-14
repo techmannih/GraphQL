3def46de2ee0c80fbb8eb004739497d57e8472cd
techmannih <manishchaudharyttt@gmail.com>
2024-01-14 21:00:46 +0530

    add rounting

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './component/Login/login';
+import Signup from './component/signup/signup';
 
 function App() {
-
   return (
-    <>
-hello
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
